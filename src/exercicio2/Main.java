fbca801a2a9e7680e81eeec7cfbdf389650d9029
Biblioteca <Biblioteca@172.19.1.35>
2026-05-19 16:17:44 -0300

    commit exercicio 2

---
+++ src/exercicio2/Main.java
@@ -0,0 +1,11 @@
+package exercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Retangulo retangulo = new Retangulo(12, 14);
+		retangulo.exibirDados();
+	}
+
+}
---
+++ src/exercicio2/Retangulo.java
@@ -0,0 +1,39 @@
+package exercicio2;
+
+public class Retangulo {
+	
+	private int comprimento;
+	private int largura;
+	
+	public Retangulo(int comprimento, int largura) {
+		this.comprimento = comprimento;
+		this.largura = largura;
+	}
+	
+	public int getComprimento() {
+		return comprimento;
+	}
+	
+	public int getLargura() {
+		return largura;
+	}
+	
+	public void setComprimento(int comprimento) {
+		this.comprimento = comprimento;
+	}
+	
+	public void setLargura(int largura) {
+		this.largura = largura;
+	}
+	
+	public void exibirDados() {
+		System.out.println(largura);
+		System.out.println(comprimento);
+	}
+	
+	
+}
+
+/*2 - Escreva um programa em Java para criar uma classe chamada Retangulo com
+as variáveis de instância privadas comprimento e largura. Forneça métodos
+públicos getter e setter para acessar e modificar essas variáveis.*/
