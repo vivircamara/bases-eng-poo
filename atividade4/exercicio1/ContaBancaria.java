295e362750a8b24068da62eed5836d4c07697f30
Biblioteca <Biblioteca@172.19.2.207>
2026-05-20 15:54:26 -0300

    commit

--- atividade4/exercicio1/ContaBancaria.java
+++ atividade4/exercicio1/ContaBancaria.java
@@ -1,5 +1,47 @@
 package exercicio1;
 
 public class ContaBancaria {
+	 String cliente;
+	 int numeroConta;
+	 int saldo;
+	 
+	 public ContaBancaria(String cliente, int numeroConta, int saldo) {
+		 this.cliente = cliente;
+		 this.numeroConta = numeroConta;
+		 this.saldo = saldo;
+		 
+	 }
+	 
+	 
+	 // Método sacar (o saldo não pode ficar negativo)
+	 // Método depositar
 
 }
+
+/*Exercício 1 – Conta Bancária: Elabore uma classe ContaBancaria, com os seguintes
+membros:
+• Cliente
+• Número da conta
+• Saldo
+• Método sacar (o saldo não pode ficar negativo)
+• Método depositar
+Agora acrescente ao projeto duas classes herdadas de ContaBancaria: ContaPoupança
+e ContaEspecial, com as seguintes características a mais:
+• Classe ContaPoupança:
+o Dia de rendimento;
+o Método “calcularNovoSaldo”, recebe a taxa de rendimento da poupança
+e atualiza o saldo.
+• Classe ContaEspecial:
+o Limite;
+o Redefinição do método sacar, permitindo saldo negativo até o valor do
+limite;
+///////////////////////////////////////////////////////////////////
+Após a implementação das classes acima, você deverá implementar uma classe
+Contas, contendo o método principal. Nesta classe, você deverá implementar:
+1. Incluir dados relativos a(s) conta(s) de um cliente;
+2. Sacar um determinado valor da(s) sua(s) conta(s);
+3. Depositar um determinado valor na(s) sua(s) conta(s);
+4. Mostrar o novo saldo do cliente, a partir da taxa de rendimento, daqueles que
+possuem conta poupança;
+5. Mostrar os dados da(s) conta(s) de um cliente.*/
+
