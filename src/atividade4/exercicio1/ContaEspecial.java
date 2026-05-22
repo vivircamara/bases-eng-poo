0a9fa746871d2b600b0da4d4d7f301ea15368333
Biblioteca <Biblioteca@UNDF0614BCE8>
2026-05-22 15:31:20 -0300

    atualizações

---
+++ src/atividade4/exercicio1/ContaEspecial.java
@@ -0,0 +1,10 @@
+package atividade4.exercicio1;
+
+public class ContaEspecial {
+
+}
+
+/*• Classe ContaEspecial:
+	o Limite;
+	o Redefinição do método sacar, permitindo saldo negativo até o valor do
+	limite;*/
---
+++ src/atividade4/exercicio1/ContaPoupanca.java
@@ -0,0 +1,33 @@
+package atividade4.exercicio1;
+
+public class ContaPoupanca extends ContaBancaria {
+	
+	private int diaRendimento;
+	
+	public ContaPoupanca(String cliente, int numeroConta, int saldo, int diaRendimento) {
+		super(cliente, numeroConta, saldo);
+		this.diaRendimento = diaRendimento;
+	}
+	
+	public double calcularNovoSaldo() {
+		 this.saldo = saldo * 1.005;
+		 return saldo;
+	}
+	
+	@Override
+	 public void exibirDados() {
+		super.exibirDados();
+		System.out.println("Dia de rendimento: " + diaRendimento); 
+
+	 }
+	 
+	
+
+}
+
+/*• Classe ContaPoupança:
+	o Dia de rendimento;
+	o Método “calcularNovoSaldo”, recebe a taxa de rendimento da poupança
+	e atualiza o saldo.*/
+
+//taxa de rendimento da poupança = míseros 0,5% 
