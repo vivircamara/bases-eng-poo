645def7177a53f6ee6817359e0ed91d4e2b9b810
vivicm <vivicm101@gmail.com>
2026-05-26 20:31:25 -0300

    primeiro commit

---
+++ src/atividade4/exercicio2/Principal.java
@@ -0,0 +1,20 @@
+package atividade4.exercicio2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*Exercício 2 – Sistema de Peças: Elabore um sistema que represente peças utilizadas
+para montar equipamentos eletrônicos, como computadores por exemplo. Tome como
+base o diagrama de classes a seguir:
+O preço da peça na superclasse é calculado pela soma do custo com o lucro. Enquanto,
+o preço da peça na subclasse é calculado a partir da soma das taxas ao preço base da
+peça.
+A partir das informações acima, você deverá primeiro identificar a superclasse,
+subclasse, os atributos, métodos e assim desenvolver o sistema.
+ */
