c2c13d69d4845af805c7da4d26fea5670c28d65c
Biblioteca <Biblioteca@UNDF0614BCE8>
2026-05-28 13:18:53 -0300

    Atividade 2, exercicio 1 finalizada

--- src/atividade2/exercicio1/Principal.java
+++ src/atividade2/exercicio1/Principal.java
@@ -1,15 +1,48 @@
 package atividade2.exercicio1;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o nome do item:");
+		String nome = scanner.nextLine();
+		
+		System.out.println("Digite o preço do item:");
+		int preco = scanner.nextInt();
+		
+		System.out.println("Digite a quantidade no estoque:");
+		int qtd = scanner.nextInt();
+		
+		System.out.println("Digite valor para retirar:");
+		int saida = scanner.nextInt();
+		
+		System.out.println("Digite valor para adicionar:");
+		int entrada = scanner.nextInt();
+		
+		scanner.close();
+		
+		
+		//Produto novoProduto = new Produto("escova", 24, 10);
+		Produto novoProduto = new Produto(nome, preco, qtd);
+		
+		novoProduto.exibirDados();
+		/*novoProduto.entradaEstoque(20);
+		novoProduto.saidaEstoque(50);
+		novoProduto.saidaEstoque(10);*/
+		
+		novoProduto.entradaEstoque(entrada);
+		novoProduto.saidaEstoque(saida);
+		novoProduto.saidaEstoque(saida);
 
 	}
 
 }
 
-//Recuperar os códigos no laptop da 206, não esquecer de enviar essa atividade
 
 /*1 – Crie um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque). 
 Em seguida: 
--- src/atividade2/exercicio1/Produto.java
+++ src/atividade2/exercicio1/Produto.java
@@ -1,5 +1,40 @@
 package atividade2.exercicio1;
 
 public class Produto {
+	
+	String nome;
+	int preco;
+	int qtd;
+	int valorEstoque;
+	
+	public Produto(String nome,	int preco, int qtd) {
+		this.nome = nome;
+		this.preco = preco;
+		this.qtd = qtd;
+	}
+	
+	public void exibirDados() {
+		int valorEstoque = preco * qtd;
+		
+		System.out.printf("item: %s\n", nome);
+		System.out.println("preço: R$" + preco);
+		System.out.println("quantidade: " + qtd);
+		System.out.println("valor total no estoque: " + valorEstoque + " reais\n");
+	}
+	
+	public void entradaEstoque(int entrada) {
+		qtd = qtd + entrada;
+		System.out.printf("Entrada realizada, +%d do seguinte ", entrada);
+		exibirDados();
+	}
+	public void saidaEstoque(int saida) {
+		if(saida > qtd) {
+			System.out.println("Estoque insuficiente para retirar essa quantidade.\n");
+		}else {
+			qtd = qtd - saida;
+			System.out.printf("Saída realizada, -%d do seguinte ", saida);
+			exibirDados();
+		}
+	}
 
 }
